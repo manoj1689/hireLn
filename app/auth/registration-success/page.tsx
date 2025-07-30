8156e8f9045463d6433008d2d008ee32468fb0f5
Userd23 <firstuser.d23@gmail.com>
2025-07-30 14:33:46 +0530

    latest code

--- app/auth/registration-success/page.tsx
+++ app/auth/registration-success/page.tsx
@@ -9,9 +9,23 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 import { CheckCircle, Mail } from "lucide-react"
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa"
 import AuthNavbar from "@/components/auth-navbar/page"
+import { useSelector } from "react-redux"
+import { RootState } from "@/lib/store"
+import { useEffect } from "react"
 
 export default function RegistrationSuccessPage() {
   const router = useRouter()
+  const { loading, error, successMessage } = useSelector((state:RootState) => state.registerStep3)
+
+  useEffect(() => {
+  if (successMessage) {
+    const timer = setTimeout(() => {
+      router.push('/dashboard')
+    }, 2000) // 2 seconds
+
+    return () => clearTimeout(timer) // cleanup
+  }
+}, [successMessage, router])
 
   return (<>
     <div>
@@ -93,12 +107,12 @@ export default function RegistrationSuccessPage() {
 
               </div>
             </div>
-            <div className="flex w-full justify-center">
+            {/* <div className="flex w-full justify-center">
               <Button onClick={() => router.push("/dashboard")} className="flex items-center gap-2">
                 Get Started
                 <FaArrowRight className="w-4 h-4" />
               </Button>
-            </div>
+            </div> */}
 
 
             <div className="text-center text-sm font-light text-gray-500">
